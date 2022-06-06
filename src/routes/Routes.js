3ba3011092b49f4312e65330c0fb7527a859d261
Arevik <arevhayrapetyan88@gmail.com>
2022-06-06 19:03:08 +0400

    fix: Fixed conflicts

--- src/routes/Routes.js
+++ src/routes/Routes.js
@@ -1,37 +1,24 @@
 import React, { Fragment } from "react";
 import { Routes, Route } from "react-router-dom";
 import Header from "../layout/header/Header";
-<<<<<<< HEAD
+
 import Products from "../pages/Products";
 import ContactUs from "../pages/ContactUs";
-import About from "../pages/About"
-import Account from "../pages/Account"
-import Dashboard from "../components/User/Dashboard"
-import ButtonComponent from "../pages/ButtonComponent";
-=======
-import Products from "../components/Card/Products";
 import About from "../pages/About";
-import ContactUs from "../pages/ContactUs";
->>>>>>> 5e308aaa4e4bcbf84f688739e91babffe6ae847b
+import Account from "../pages/Account";
+import Dashboard from "../components/User/Dashboard";
+import ButtonComponent from "../pages/ButtonComponent";
 
 const PageRoutes = () => {
   return (
     <Fragment>
-      <Header/>
-    <Routes>
-      <Route path="/products" element={<Products />} />
-<<<<<<< HEAD
-      <Route path="/contact" element={<ContactUs />} />
-      <Route path="/about" element={<About />} />
-      <Route path="/account" element={<Account />}>
-          <Route path="dashboard" element={<Dashboard />} />
-        </Route>
-=======
-      <Route path="/about" element={<About />} />
-      <Route path="/contact" element={<ContactUs />} />
-
->>>>>>> 5e308aaa4e4bcbf84f688739e91babffe6ae847b
-    </Routes>
+      <Header />
+      <Routes>
+        <Route path="/products" element={<Products />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<ContactUs />} />
+        <Route path="/account" element={<Account />} />
+      </Routes>
     </Fragment>
   );
 };
