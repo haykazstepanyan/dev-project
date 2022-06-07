3ed40150cd855398932eb3ac7862019f7aed78b3
Lucine1999 <lusine.mardan@gmail.com>
2022-06-07 16:36:44 +0400

    wishlist navigate

--- src/layout/header/HeaderMiddle.js
+++ src/layout/header/HeaderMiddle.js
@@ -1,7 +1,7 @@
 import React from "react";
 import {
-  ClickableDropdown,
-  HoverableDropdown,
+    ClickableDropdown,
+    HoverableDropdown,
 } from "../../components/dropdown";
 import Grid from "@mui/material/Grid";
 import Container from "@mui/system/Container";
@@ -14,75 +14,89 @@ import styles from "./styles";
 import { createUseStyles } from "react-jss";
 import { useState } from "react";
 import logo from "../../assets/images/logo.png";
+import { useNavigate } from "react-router-dom";
 
 const useStyles = createUseStyles(styles);
 
 const HeaderMiddle = () => {
-  const [category, setCategory] = useState("All categories");
-  const [inputValue, setInputValue] = useState("");
+    const navigate = useNavigate();
 
-  const categoriesArray = [
-    "All categories",
-    "Category 1",
-    "Category 2",
-    "Category 3 for kids",
-    "Category 4",
-    "Category 5",
-  ];
-  const accountData = ["My account", "Shopping cart", "Wishlist"];
+    const [category, setCategory] = useState("All categories");
+    const [inputValue, setInputValue] = useState("");
 
-  const classes = useStyles();
+    const navigateFavPage = () => {
+        navigate("/wishlist");
+    };
 
-  const handleCategory = (value) => {
-    categoriesArray.includes(value) && setCategory(value);
-  };
-  const handleInputChange = (e) => {
-    setInputValue(e.target.value);
-  };
-  return (
-    <div className={classes.headerParts}>
-      <Container maxWidth="lg">
-        <Grid
-          container
-          direction="row"
-          justifyContent="space-between"
-          alignItems="center"
-        >
-          <Grid item sm={2} className={classes.logoBox}>
-            <img className={classes.pageLogo} src={logo} alt="Logo" />
-          </Grid>
-          <Grid item sm={8} className={classes.filterContainer}>
-            <div className={classes.filterContainerContent}>
-              <ClickableDropdown
-                options={categoriesArray}
-                icon={<KeyboardArrowDownIcon />}
-                change={handleCategory}
-                value={category}
-              />
-              <div className={classes.searchBox}>
-                <input
-                  className={classes.searchInput}
-                  value={inputValue}
-                  onChange={handleInputChange}
-                  placeholder="Search product..."
-                />
-              </div>
-              <SearchIcon className={classes.searchIcon} />
-            </div>
-          </Grid>
-          <Grid item sm={2} className={classes.iconsContainer}>
-            <HoverableDropdown
-              value={<PeopleOutlineIcon />}
-              list={accountData}
-            />
+    const categoriesArray = [
+        "All categories",
+        "Category 1",
+        "Category 2",
+        "Category 3 for kids",
+        "Category 4",
+        "Category 5",
+    ];
+    const accountData = ["My account", "Shopping cart", "Wishlist"];
 
-            <FavoriteBorderIcon className={classes.icons} />
-            <WorkOutlineIcon className={classes.icons} />
-          </Grid>
-        </Grid>
-      </Container>
-    </div>
-  );
+    const classes = useStyles();
+
+    const handleCategory = (value) => {
+        categoriesArray.includes(value) && setCategory(value);
+    };
+    const handleInputChange = (e) => {
+        setInputValue(e.target.value);
+    };
+    return (
+        <div className={classes.headerParts}>
+            <Container maxWidth="lg">
+                <Grid
+                    container
+                    direction="row"
+                    justifyContent="space-between"
+                    alignItems="center"
+                >
+                    <Grid item sm={2} className={classes.logoBox}>
+                        <img
+                            className={classes.pageLogo}
+                            src={logo}
+                            alt="Logo"
+                        />
+                    </Grid>
+                    <Grid item sm={8} className={classes.filterContainer}>
+                        <div className={classes.filterContainerContent}>
+                            <ClickableDropdown
+                                options={categoriesArray}
+                                icon={<KeyboardArrowDownIcon />}
+                                change={handleCategory}
+                                value={category}
+                            />
+                            <div className={classes.searchBox}>
+                                <input
+                                    className={classes.searchInput}
+                                    value={inputValue}
+                                    onChange={handleInputChange}
+                                    placeholder="Search product..."
+                                />
+                            </div>
+                            <SearchIcon className={classes.searchIcon} />
+                        </div>
+                    </Grid>
+                    <Grid item sm={2} className={classes.iconsContainer}>
+                        <HoverableDropdown
+                            value={<PeopleOutlineIcon />}
+                            list={accountData}
+                        />
+
+                        <FavoriteBorderIcon
+                            onClick={navigateFavPage}
+                            className={classes.icons}
+                        />
+                        <WorkOutlineIcon className={classes.icons} />
+                    </Grid>
+                </Grid>
+            </Container>
+        </div>
+    );
 };
 
 export default HeaderMiddle;
