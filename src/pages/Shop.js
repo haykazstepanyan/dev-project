bc365abbeec288043cfbe880973f57ba0eb41bda
Lucine1999 <lusine.mardan@gmail.com>
2022-06-12 20:42:50 +0400

    last changes

--- src/pages/Shop.js
+++ src/pages/Shop.js
@@ -9,54 +9,62 @@ import { Link } from "react-router-dom";
 import ProductItem from "../components/product";
 
 const Shop = () => {
-  const [products, setProducts] = useState([]);
-  const [productsbyPages, setProductsbyPages] = useState([]);
-  const [start, setStart] = useState(0);
+    const [products, setProducts] = useState([]);
+    const [productsbyPages, setProductsbyPages] = useState([]);
+    const [start, setStart] = useState(0);
 
-  useEffect(() => {
-    getFakeProductsData().then((productsData) =>
-      setProducts((prev) => [...prev, ...productsData])
+    useEffect(() => {
+        getFakeProductsData().then((productsData) =>
+            setProducts((prev) => [...prev, ...productsData])
+        );
+    }, []);
+
+    useEffect(() => {
+        if (products.length > 0) {
+            setProductsbyPages([...products].slice(start, start + 9));
+        }
+    }, [products, start]);
+
+    const gotoPage = (_, page) => {
+        setStart(page * 9 - 9);
+    };
+
+    return (
+        <>
+            <Layout />
+            <Banner name="Shop" />
+            <Container maxWidth="lg">
+                <Grid container spacing={2}>
+                    <Grid item md={3}></Grid>
+                    <Grid item md={9}>
+                        <Grid container spacing={2}>
+                            {productsbyPages &&
+                                productsbyPages.map(
+                                    ({ id, title, images, price }) => (
+                                        <Grid item sm={4} key={id}>
+                                            <Link
+                                                to={`/product/${id}`}
+                                                style={{ margin: "15px" }}
+                                            >
+                                                <ProductItem
+                                                    title={title}
+                                                    image={images[0]}
+                                                    price={price}
+                                                />
+                                            </Link>
+                                        </Grid>
+                                    )
+                                )}
+                        </Grid>
+                    </Grid>
+                </Grid>
+            </Container>
+
+            <Pagination
+                count={Math.ceil(products.length / 9)}
+                onChange={gotoPage}
+            />
+        </>
     );
-  }, []);
-
-  useEffect(() => {
-    if (products.length > 0) {
-      setProductsbyPages([...products].slice(start, start + 9));
-    }
-  }, [products, start]);
-
-  const gotoPage = (_, page) => {
-    setStart(page * 9 - 9);
-  };
-
-  return (
-    <>
-      <Layout />
-      <Banner name="Shop" />
-      <Container maxWidth="lg">
-        <Grid container spacing={2}>
-          <Grid item md={3}></Grid>
-          <Grid item md={9}>
-            <Grid container spacing={2}>
-              {productsbyPages &&
-                productsbyPages.map(({ id, title, images, price }) => (
-                  <Grid item sm={4} key={id}>
-                    <Link to={`/product/${id}`} style={{ margin: "15px" }}>
-                      <ProductItem
-                        title={title}
-                        image={images[0]}
-                        price={price}
-                      />
-                    </Link>
-                  </Grid>
-                ))}
-            </Grid>
-          </Grid>
-        </Grid>
-      </Container>
-
-      <Pagination count={Math.ceil(products.length / 9)} onChange={gotoPage} />
-    </>
-  );
 };
 export default Shop;
