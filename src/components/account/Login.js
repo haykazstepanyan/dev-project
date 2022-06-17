0c43df2910d28618440cb2b9aca47706a93f6e37
Lucine1999 <lusine.mardan@gmail.com>
2022-06-17 11:07:25 +0400

    login component change

--- src/components/account/Login.js
+++ src/components/account/Login.js
@@ -1,35 +1,32 @@
 import { Container, Box } from "@mui/material";
-import Layout from "../../layout/Layout";
-import Banner from "../common/Banner";
+// import Layout from "../../layout/Layout";
+// import Banner from "../common/Banner";
 import Form from "../form/FormControlBlock";
 import { loginStyles } from "./styles";
 
 function Logout() {
   const classes = loginStyles();
   return (
-    <Layout>
-      <Banner name="My Account" />
-      <Container className={classes.container}>
-        <Box className={classes.boxContainer}>
-          <Container>
-            <h2 className={classes.title}>Login</h2>
-            <Form
-              nameInput="Username or email *"
-              passwordInput="Passwords *"
-              loginType
-            />
-          </Container>
-          <Container>
-            <h2 className={classes.title}>Register</h2>
-            <Form
-              nameInput="Email address *"
-              passwordInput="Passwords *"
-              loginType={false}
-            />
-          </Container>
-        </Box>
-      </Container>
-    </Layout>
+    <Container className={classes.container}>
+      <Box className={classes.boxContainer}>
+        <Container>
+          <h2 className={classes.title}>Login</h2>
+          <Form
+            nameInput="Username or email *"
+            passwordInput="Passwords *"
+            loginType
+          />
+        </Container>
+        <Container>
+          <h2 className={classes.title}>Register</h2>
+          <Form
+            nameInput="Email address *"
+            passwordInput="Passwords *"
+            loginType={false}
+          />
+        </Container>
+      </Box>
+    </Container>
   );
 }
 
