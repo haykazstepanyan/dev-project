ffc94bb425e222e5565d3d8aff67a4d1e69ac882
Garik Kyarunts <kyaruntsg@gmail.com>
2022-06-26 14:57:45 +0400

    refactor(added style dinamically)

--- src/pages/ProductView.js
+++ src/pages/ProductView.js
@@ -14,11 +14,54 @@ import FacebookIcon from "@mui/icons-material/Facebook";
 import PinterestIcon from "@mui/icons-material/Pinterest";
 import LinkedInIcon from "@mui/icons-material/LinkedIn";
 import { getProductById } from "../helpers/helpers";
+import { productViewStyles } from "./styles";
 
+/*
+  product_add-to-wishlist_container: { 
+    marginTop: 3,
+  },
+  
+  product_add-to-wishlist_text: {
+    fontSize: 12,
+  },
+  
+  product_category_container: {
+    marginTop: 3,
+  },
+
+  product_category_text: {
+    marginRight: 4,
+  },
+
+  social-network-icons_container: {
+    display: "flex",
+    justifyContent: "space-between",
+    width: 100,
+  },
+
+  fb_icon: {
+    cursor: "pointer", 
+    color: "#4267B2",
+  },
+
+  pinterest_icon: { 
+    cursor: "pointer", 
+    color: "#E60023",
+  },
+
+  linkedIn_icon: { 
+    cursor: "pointer", 
+    color: "#0072b1",
+  },
+
+ }
+
+
+*/
 function Product() {
+  const classes = productViewStyles();
   const [product, setProduct] = useState({ category: {} });
   const { productId } = useParams();
-  //  console.log(product, productId);
   useEffect(() => {
     async function getProduct() {
       const { data: productData } = await getProductById(productId);
@@ -34,47 +77,62 @@ function Product() {
 
   return (
     <Container maxWidth="lg">
-      <Grid container alignItems="center">
-        <Grid item md={6} sm={12} padding={10}>
+      <Grid
+        container
+        // className={classes["container_grid-container"]}
+        alignItems="center"
+      >
+        <Grid
+          item
+          md={6}
+          sm={12}
+          // className={classes["grid-item_img_container"]}
+          padding={10}
+        >
           <Box component="div">
-            <img
-              src={`${product.productImg}`}
-              alt="some img"
-              style={{ maxWidth: "100%" }}
-            />
+            <img src={`${product.productImg}`} alt="some img" maxWidth="100%" />
           </Box>
         </Grid>
-        <Grid item md={6} sm={10} padding={5}>
-          <Box maxWidth="100%">
-            <Box component="div" marginBottom={3}>
+        <Grid
+          item
+          md={6}
+          sm={10}
+          // className={classes["grid-item_content"]}
+          padding={5}
+        >
+          <Box
+            //  className={classes["grid-item_content_container"]}
+            maxWidth="100%"
+          >
+            <Box
+              component="div"
+              // className={classes["product-name-price_container"]}
+              marginBottom={3}
+            >
               <Typography component="h4" variant="h5">
                 {product.name}
               </Typography>
             </Box>
-            <Box component="div" marginBottom={3}>
+            <Box
+              component="div"
+              // className={classes["product-name-price_container"]}
+              marginBottom={3}
+            >
               <Typography>
-                <span
-                  className="current-price"
-                  style={{ fontWeight: 500, fontSize: 23, color: "#79a206" }}
-                >
+                <span className={classes["product_current-price"]}>
                   £{product.price}
                 </span>
                 {!!product.discount && (
-                  <span
-                    className="old-price"
-                    style={{
-                      textDecoration: "line-through",
-                      fontWeight: 400,
-                      fontSize: 20,
-                      marginLeft: 10,
-                    }}
-                  >
+                  <span className={classes["product_old-price"]}>
                     £{product.price + (product.price * product.discount) / 100}
                   </span>
                 )}
               </Typography>
             </Box>
-            <Box marginBottom={6}>
+            <Box
+              // className={classes.product_description_container}
+              marginBottom={6}
+            >
               <Typography component="p">{product.description}</Typography>
             </Box>
 
@@ -88,38 +146,50 @@ function Product() {
                 defaultValue={1}
                 InputProps={{ inputProps: { min: 1, max: 10 } }}
               />
-              <Box sx={{ display: "inline", marginLeft: 3 }}>
+              <Box
+                // className={classes["product_form-button"]}
+                sx={{ display: "inline", marginLeft: 3 }}
+              >
                 {/* Redux CART state needed and DB table CART needed!!! */}
                 <Button variant="contained">Add To Cart</Button>
               </Box>
             </Box>
-            <Box marginTop={3}>
+
+            <Box
+              // className={classes["product_add-to-wishlist_container"]}
+              marginTop={3}
+            >
               {/* Redux WISHLIST state needed and DB table WISHLIST needed!!! */}
               <Link
                 href="/"
-                sx={{ fontSize: 12 }}
+                // className={classes["product_add-to-wishlist_text"]}
+                fontSize={12}
                 onClick={handleAddToWishlist}
               >
                 +Add to wishlist
               </Link>
             </Box>
-            <Box marginTop={3}>
+            <Box
+              // className={classes.product_category_container}
+              sx={{ marginTop: 3 }}
+            >
               <Typography>
-                <b style={{ marginRight: 4 }}>Category: </b>
+                <b className={classes.product_category_text}>Category:</b>
                 <span>{product.category.name}</span>
               </Typography>
             </Box>
             <Box
-              marginTop={3}
+              //  className={classes["social-network-icons_container"]}
               sx={{
                 display: "flex",
                 justifyContent: "space-between",
                 width: 100,
+                marginTop: 3,
               }}
             >
-              <FacebookIcon sx={{ cursor: "pointer", color: "#4267B2" }} />
-              <PinterestIcon sx={{ cursor: "pointer", color: "#E60023" }} />
-              <LinkedInIcon sx={{ cursor: "pointer", color: "#0072b1" }} />
+              <FacebookIcon className={classes.fb_icon} />
+              <PinterestIcon className={classes.pinterest_icon} />
+              <LinkedInIcon className={classes.linkedIn_icon} />
             </Box>
           </Box>
         </Grid>
