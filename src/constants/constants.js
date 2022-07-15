1361429565715d6417ce548507187abf4408eda3
Garik Kyarunts <kyaruntsg@gmail.com>
2022-07-15 17:58:35 +0400

    bug(constant)

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -10,6 +10,7 @@ export const colors = {
   red: "#DC0F0F",
 };
 
+export const BASE_URL = "http://localhost:5000";
 export const TABLE_TITLES = ["Date", "Status", "Total", "Products", "Actions"];
 export const NAV_LINKS = [
   { route: "/", name: "Home" },
